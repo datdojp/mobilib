403abf4e8e9e0739415560fc3e5210e9fb51f5e8
Dat Do <datdvt@mobilus.co.jp>
2015-05-29 12:03:40 +0900

    Improve MblYieldLayout
    1. Throw runtime exceptions
    2. Initialize views in onFinishInflate() instead of post() so that it
    won't block the flow
    3. Support multiple children instead of just one

--- Mobilib/src/com/datdo/mobilib/widget/MblYieldLayout.java
+++ Mobilib/src/com/datdo/mobilib/widget/MblYieldLayout.java
@@ -5,7 +5,7 @@ import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import com.datdo.mobilib.R;
 import com.datdo.mobilib.util.MblUtils;
@@ -13,49 +13,46 @@ import com.datdo.mobilib.util.MblUtils;
 /**
  * Created by datdvt on 2015/05/28.
  */
-public class MblYieldLayout extends FrameLayout {
+public class MblYieldLayout extends LinearLayout {
 
-    public MblYieldLayout(Context context) {
-        super(context);
-    }
+    private int mYieldId;
+    private int mLayoutId;
 
     public MblYieldLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
         init(context, attrs);
     }
 
-    public MblYieldLayout(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init(context, attrs);
+    private void init(Context context, AttributeSet attrs) {
+        TypedArray ta = context.getTheme().obtainStyledAttributes(attrs, R.styleable.MblYieldLayout, 0, 0);
+        mYieldId = ta.getResourceId(R.styleable.MblYieldLayout_yieldId, -1);
+        mLayoutId = ta.getResourceId(R.styleable.MblYieldLayout_layoutId, -1);
+        if (mYieldId < 0 || mLayoutId < 0) {
+            throw new RuntimeException(MblYieldLayout.class + ": layoutId or yieldId is missing");
+        }
     }
 
-    private void init(Context context, AttributeSet attrs) {
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
 
-        TypedArray ta = context.getTheme().obtainStyledAttributes(attrs, R.styleable.MblYieldLayout, 0, 0);
-        final int yieldId = ta.getResourceId(R.styleable.MblYieldLayout_yieldId, -1);
-        final int layoutId = ta.getResourceId(R.styleable.MblYieldLayout_layoutId, -1);
-
-        if (yieldId < 0 || layoutId < 0) {
-            return;
+        View[] children = new View[getChildCount()];
+        for (int i = 0; i < getChildCount(); i++) {
+            children[i] = getChildAt(i);
+        }
+        View layout = MblUtils.getLayoutInflater().inflate(mLayoutId, null);
+        if (layout == null) {
+            throw new RuntimeException(MblYieldLayout.class + ": layoutId not found");
+        }
+        ViewGroup yieldLayout = (ViewGroup) layout.findViewById(mYieldId);
+        if (yieldLayout == null) {
+            throw new RuntimeException(MblYieldLayout.class + ": yieldId not found");
         }
 
-        MblUtils.getMainThreadHandler().post(new Runnable() {
-            @Override
-            public void run() {
-
-                if (getChildCount() == 0) {
-                    return;
-                }
-
-                View child = getChildAt(0);
-                View layout = MblUtils.getLayoutInflater().inflate(layoutId, null);
-                ViewGroup yieldLayout = (ViewGroup) layout.findViewById(yieldId);
-
-                removeView(child);
-                yieldLayout.addView(child);
-                layout.setLayoutParams(getLayoutParams());
-                addView(layout);
-            }
-        });
+        for (View child : children) {
+            removeView(child);
+            yieldLayout.addView(child);
+        }
+        addView(layout);
     }
 }
