8c205a5ebec5f2d5e6b7b04fd7de86d445197a08
Dat Do <datdo@rjc.co.jp>
2016-04-20 17:11:55 +0900

    DBHelper: user Application context to prevent memory leak

--- Mobilib/src/com/datdo/mobilib/db/DBBase.java
+++ Mobilib/src/com/datdo/mobilib/db/DBBase.java
@@ -2,13 +2,15 @@ package com.datdo.mobilib.db;
 
 import com.datdo.mobilib.util.MblUtils;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
 public abstract class DBBase {
     private static SQLiteDatabase sDb;
     protected static SQLiteDatabase getDatabase() {
         if (sDb == null) {
-            sDb = DBHelper.getInstance(MblUtils.getCurrentContext()).getWritableDatabase();
+            Context context = MblUtils.getCurrentContext().getApplicationContext();
+            sDb = DBHelper.getInstance(context).getWritableDatabase();
         }
         return sDb;
     }
