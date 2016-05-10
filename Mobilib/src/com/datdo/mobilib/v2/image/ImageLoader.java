ecccec9f075e1c340a41ef495fd54743f9afed63
Dat Do <datdo@rjc.co.jp>
2016-05-10 10:02:44 +0900

    ImageLoader: remove redundant lines

--- Mobilib/src/com/datdo/mobilib/v2/image/ImageLoader.java
+++ Mobilib/src/com/datdo/mobilib/v2/image/ImageLoader.java
@@ -37,8 +37,6 @@ public class ImageLoader {
 
     public LoadRequest forOneImageView(Context context) {
         return with(context)
-                .enableProgressView(false)
-                .enableFadingAnimation(true)
                 .scaleToImageViewSizes(true)
                 .cropBitmapToImageViewSizes(false)
                 .serialized(false)
